f01eae4b1d43727340afbddca2c3a3502d7911f5
Igor Pahota <eto.pochtovaya@gmail.com>
2020-03-15 17:25:29 +0300

    generic

---
+++ generic.js
@@ -0,0 +1,9 @@
+var arrayOfNumbers = [1, 1, 2, 3, 5];
+var arrayOfStrings = ['Hello', 'Igor'];
+// function reverse<T>(array: T[]): T[] {
+//     return array.reverse()
+// }
+function reverse(array) {
+    return array.reverse();
+}
+console.log(reverse(arrayOfStrings));
