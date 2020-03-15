fdc0148c9b3fad31c03b62a82c955042e0f3c899
Igor Pahota <eto.pochtovaya@gmail.com>
2020-03-15 17:19:42 +0300

    guards

---
+++ guards.ts
@@ -0,0 +1,36 @@
+import RuntimeError = WebAssembly.RuntimeError;
+
+function strip(x: string | number) {
+    if (typeof x === 'number') {
+        return x.toFixed(2)
+    }
+    return x.trim()
+}
+
+class MyResponse {
+    header = 'response header';
+    result = 'response result'
+}
+
+class MyError {
+    header = 'error header';
+    message = 'error message'
+}
+
+function handleError(res: MyResponse | MyError) {
+    if(res instanceof MyResponse) {
+        return {
+            info: res.header + res.result
+        }
+    } else {
+        return {
+            info: res.header + res.message
+        }
+    }
+}
+
+type AlertType = 'success' | 'danger' | 'warning'
+
+function setAlertType(type: AlertType) {
+
+}
